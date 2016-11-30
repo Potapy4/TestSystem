0c22f2ba1e7433ce65e1c8312794b79a670a1983
Restart <roman-skeba@mail.ru>
2016-11-30 15:24:46 +0200

    update

--- ProgramLogic/User.cs
+++ ProgramLogic/User.cs
@@ -13,30 +13,21 @@ namespace ProgramLogic
         public string Email { get; private set; }
         public string PhoneNumber { get; private set; }
         public string Password { get; private set; }
-        public string Email { get; private set; }
         public DateTime Birthday { get; private set; }
         private bool isAdmin;
 
-<<<<<<< HEAD
-        public User(string Firstname, string Lastname, string Username, string Password, string Email, DateTime Birthday, bool isAdmin)
-=======
         public User(string Firstname, string Lastname, string Username, DateTime Date, string City, string Sex, string Email, string PhoneNumber, string Password, bool isAdmin=false)
->>>>>>> refs/heads/Roman
         {
             this.Firstname = Firstname;
             this.Lastname = Lastname;
             this.Username = Username;
             this.Date = Date;
             this.Password = Password;
-<<<<<<< HEAD
             this.Email = Email;
-            this.Birthday = Birthday;
-=======
+            this.Date = Date;
             this.City = City;
             this.Sex = Sex;
-            this.Email = Email;
             this.PhoneNumber = PhoneNumber;
->>>>>>> refs/heads/Roman
             this.isAdmin = isAdmin;
         }
     }
