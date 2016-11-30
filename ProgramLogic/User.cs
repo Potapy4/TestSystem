ee06e086c2c7a4ff4a05f2f3ec7cf1419e854f19
Restart <roman-skeba@mail.ru>
2016-11-30 15:13:48 +0200

    update

--- ProgramLogic/User.cs
+++ ProgramLogic/User.cs
@@ -5,15 +5,23 @@
         public string Firstname { get; private set; }
         public string Lastname { get; private set; }
         public string Username { get; private set; }
+        public string City { get; private set; }
+        public string Sex { get; private set; }
+        public string Email { get; private set; }
+        public string PhoneNumber { get; private set; }
         public string Password { get; private set; }
         private bool isAdmin;
 
-        public User(string Firstname, string Lastname, string Username, string Password, bool isAdmin)
+        public User(string Firstname, string Lastname, string Username, string City, string Sex, string Email, string PhoneNumber, string Password, bool isAdmin=false)
         {
             this.Firstname = Firstname;
             this.Lastname = Lastname;
             this.Username = Username;
             this.Password = Password;
+            this.City = City;
+            this.Sex = Sex;
+            this.Email = Email;
+            this.PhoneNumber = PhoneNumber;
             this.isAdmin = isAdmin;
         }
     }
